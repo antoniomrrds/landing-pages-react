844ffde587b221a11c2055d64688a9f21986b135
antoniomrrds <arqueiroverd@gmail.com>
2022-11-09 00:08:46 -0300

    config snippet

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,6 @@ import { theme } from './styles/theme';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
